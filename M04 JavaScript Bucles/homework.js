dce4c8bc9a6d8d0ba5284cac26ea65657671584e
Nestor <nesti_dj@hotmail.com>
2023-05-10 11:14:17 -0300

    Homework completo1

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -202,7 +202,7 @@ function tieneTresDigitos(num) {
 
 function doWhile(num) {
    // Implementar una función que aumente el valor recibido en 5 hasta un límite de 8 veces.
-   // Retornar el valor final.
+   // Retornar el valor final. 
    // Utilizar el bucle Do-While.
    // Tu código:
    var i = 0
